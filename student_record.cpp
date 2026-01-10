71446cbc441b07ed361ee336f1e2b3791bbb1877
Vinod-S12 <vinodsrinivas1112@gmail.com>
2026-01-10 13:57:37 +0530

    this is based on student survey by using common all the validation in single program

--- student_record.cpp
+++ student_record.cpp
@@ -11,10 +11,16 @@ private:
     int marks[5];
     int total;
     float avg;
+    bool validemail=false;
+    bool Upper =false;
+    bool Lower = false;
+    bool number =false;
+    bool specialchar= false;
+    
 
 public:
     void StudentDetail() {
-        cout << "Enter Student Name: ";
+        cout << "Enter Student Name (length should max 5 and min 20): ";
         cin >> name;
 
         if (name.length() < 5 || name.length() > 50) {
@@ -31,18 +37,55 @@ public:
         cout << "Enter Email: ";
         cin >> email;
 
-        if (email.find('@') == string::npos) {
-            cout << "Invalid Email\n";
-            return;
+       for(int i=0;i<email.length();i++)
+       {
+        if(email[i]=='@')
+        {
+            validemail=true;
+            break;
         }
+        else{
+           validemail=false;
+        }
+       }
 
-        cout << "Enter Password (min 8 chars): ";
+        cout << "Enter Password (min 8 chars) only allowed char @ $# ^: ";
         cin >> password;
 
-        if (password.length() < 8) {
-            cout << "Weak Password\n";
-            return;
+        if (password.length() <= 8 ) {
+
+            for (int i = 0; i< password.length();i++)
+            {
+                if (password[i]>=97 && password[i]<=122)
+                {
+                    //cout << "LOwer "<<endl;
+                    Lower = true;
+                }
+                else if (password[i]>= 65 && password[i] <= 90 )
+                {
+                   // cout << "Upper"<<endl;
+                    Upper = true;
+                }
+                else if (password[i]>=48 &&password[i]<=57)
+                {
+                   // cout << "number "<<endl;
+                    number = true;
+                }
+                else if (password[i] == '@' || password[i] == '$' ||password[i] == '^' || password[i] == '#')
+                {
+                   // cout << "specialchar "<<endl;
+                    specialchar = true;
+                }
+
+               
+            }
+           if (!Upper || !Lower || !specialchar || !number)
+           {
+                    cout << "Password must be contain special char ,Uppercase ,Lowercase, special char !"<< endl;
+                    return;
+           }
         }
+        
 
         cout << "Enter College Fee: ";
         cin >> fee;
@@ -63,7 +106,10 @@ public:
         cout << "Name: " << name << endl;
         cout << "Age: " << age << endl;
         cout << "Mobile: " << mobile << endl;
-        cout << "Email: " << email << endl;
+        if(validemail){
+           cout << "Email: " << email << endl;
+        }
+       
         cout << "Fee: " << fee << endl;
         cout << "Total Marks: " << total << endl;
         cout << "Average: " << avg << endl;
